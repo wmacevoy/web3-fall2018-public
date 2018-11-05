b6f2bb5304cdf61497dda7bf555943fd5dbe0d20
Warren MacEvoy <wmacevoy@gmail.com>
2018-11-05 14:48:41 -0700

    axios example

---
+++ vm/hello2/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    {{ info }}
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'App',
+    data () {
+	return {
+	    info:   null
+	}
+    },
+    mounted () {
+	axios
+	    .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+	    .then(response => (this.info = response))
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
